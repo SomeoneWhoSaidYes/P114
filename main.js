8327ebabe718130adebe913bb6ec74609df5e3c8
SomeoneWhoSaidYes <92833774+SomeoneWhoSaidYes@users.noreply.github.com>
2022-07-10 13:31:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
